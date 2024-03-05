6af0ca77f1eb3f11c55ae5aec9ed0ff8aaa5507b
Petr Valenta <petr.valenta.92@gmail.com>
2024-03-05 22:29:59 +0100

    Switch img element for svg

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,4 @@
 import capeVerde from "../images/capeVerde.jpg";
-import pinIcon from "../images/pin.svg";
 
 
 export default function Card() {
@@ -8,18 +7,19 @@ export default function Card() {
             <img className="card__photo" src= { capeVerde } alt="Sand beach with trees" />
             <div className="card__info">
                 <div className="info__destination">
-                    <img src= { pinIcon } alt="Pin icon" />
+                    <svg width="7" height="10" viewBox="0 0 7 10" fill="#F55A5A" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M3.57866 5.25002C2.65532 5.25002 1.90837 4.50221 1.90837 3.57965C1.90837 2.65757 2.65532 1.90911 3.57866 1.90911C4.50167 1.90911 5.24927 2.65757 5.24927 3.57965C5.24927 4.50221 4.50167 5.25002 3.57866 5.25002ZM3.48394 0C1.54227 0 0 1.63678 0 3.65569C0 6.40791 2.95078 9.25191 2.95078 9.25191C3.34777 9.62783 3.59437 9.65847 4.01646 9.25191C4.01646 9.25191 7 6.40791 7 3.65569C7 1.63678 5.42544 0 3.48394 0Z"/>
+                    </svg>
                     <p className="uppercase">Cape Verde</p>
-                    <a className="cl-gray" href="https://maps.app.goo.gl/EkYeTA5QyEfuKjWc9">View on Google Maps</a>
+                    <a className="cl-gray" href="https://maps.app.goo.gl/EkYeTA5QyEfuKjWc9">
+                        View on Google Maps
+                    </a>
                 </div>
                 <h1 className="info__title">Santa Maria</h1>
                 <p className="info__duration">Oct 01, 2019 - Oct 21, 2019</p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetuer 
-                    adipiscing elit. Mauris suscipit, ligula 
-                    sit amet pharetra semper, nibh ante cursus 
-                    purus, vel sagittis velit mauris vel metus. 
-                    Aliquam ante. 
+                <p className="info__descript">
+                    Our honeymoon! This small town is situated on the Sal island which 
+                    is full of sand and salt. Even so this place with endless beaches and clear ocean.
                 </p>
             </div>
         </div>
