8123bc0b1522a1f6b9a458fa9d5e4301bb9ae0c2
Petr Valenta <petr.valenta.92@gmail.com>
2024-03-06 00:02:24 +0100

    Add map function to render every single Card component for data from data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import data from "./data"
 import './App.css';
 
 function App() {
+
+  const cardElement = data.map( card => {
+    return (
+      <Card 
+        key = { card.id }
+        card = { card }
+      />
+    )
+  })
+
   return (
     <>
       <Navbar />
       <section>
-        <Card />
-        <Card />
-        <Card />
+        { cardElement }
       </section>
     </>
   )
